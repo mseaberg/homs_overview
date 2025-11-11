10d998740471647b717036d2e9cc5838cb404aed
Nick Waters <nrw@psbuild-rhel7-01.slac.stanford.edu>
2025-11-11 11:24:10 -0800

    ENH: add an individual offset mirror launch script

---
+++ launch_offset_ui.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export PCDS_CONDA_VER=6.0.1
+source /cds/group/pcds/pyps/conda/pcds_conda 
+
+echo $1
+launcher="$(realpath $0)"
+export LAUNCHER_DIR="$(dirname ${launcher})"
+app="${LAUNCHER_DIR}/homs_overview/mirrorScreen.py"
+
+pydm --hide-menu-bar --hide-nav-bar -m $1 ${app} &
