5d6ce8784a7f0d4057ed4f5ca9a7e11d5e798622
Nick Waters <nrw@psbuild-rhel7-01.slac.stanford.edu>
2025-11-11 11:28:33 -0800

    MNT: remove debug echo

--- launch_offset_ui.sh
+++ launch_offset_ui.sh
@@ -3,7 +3,6 @@
 export PCDS_CONDA_VER=6.0.1
 source /cds/group/pcds/pyps/conda/pcds_conda 
 
-echo $1
 launcher="$(realpath $0)"
 export LAUNCHER_DIR="$(dirname ${launcher})"
 app="${LAUNCHER_DIR}/homs_overview/mirrorScreen.py"
